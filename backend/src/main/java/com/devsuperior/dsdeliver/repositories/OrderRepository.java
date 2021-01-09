5abdfd913037f210522e12b78cb98a44895a453c
root <svsjoffer@gmail.com>
2021-01-09 18:02:31 -0400

    GET /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/repositories/OrderRepository.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/repositories/OrderRepository.java
@@ -2,6 +2,16 @@ package com.devsuperior.dsdeliver.repositories;
 
 import com.devsuperior.dsdeliver.entities.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-public interface OrderRepository extends JpaRepository <Order, Long> {
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    @Query("SELECT DISTINCT obj FROM Order obj JOIN FETCH obj.products"
+            + " WHERE obj.status = 0 order by obj.moment asc ")
+    List<Order> findOrdersWithProducts();
+
 }
